f7b37ec391d7158f1f93241d2e17a4a035ae4e72
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-27 10:10:36 -0500

    Update main.js

--- Auth/main.js
+++ Auth/main.js
@@ -3,8 +3,9 @@ const fs = require("fs");
 
 // Files
 const Crud = require("./crud.js");
+const eCrud = require("./functions.js");
 
-exports.login = function(user, pass) {
+exports.login = function(user, pass, ip) {
     if(user.length === 0 || pass.length === 0) {
         return "[x] Error, Invalud argument value!";
     }
@@ -19,6 +20,7 @@ exports.login = function(user, pass) {
     let db_pw = get_user.split(",")[2];
 
     if(user === db_user || pass === db_pw) {
+        Crud.LogSession(user, ip)
         return "[+] Successfully logged in, Welcome: " + user;
     } else {
         return "[x] Error, Username or password seem to be incorrect!. Try again";
