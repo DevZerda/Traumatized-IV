5474d6bb3b2dca4adb6cd951b528bc02ea9d571c
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-27 14:25:43 -0500

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,5 @@ const Crud = require("./Auth/crud.js");
 console.log(eCrud.PremiumCount());
 
 console.log(Crud.User("nano"))
+
+console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
