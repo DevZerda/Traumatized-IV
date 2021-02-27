ea11ef46434e0811711fdf7ffdfacc98d1fc02e6
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-27 07:23:45 -0500

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,12 +1,28 @@
+// Modules
 const Net = require('net');
-var fs = require('fs');
+const fs = require('fs');
 const f = require("node-fetch");
-const { exec } = require('child_process');
+const exec = require('child_process');
 const EventEmitter = require("events");
-const port = process.argv[2];
-const server = new Net.Server();
+const Crud = require("../Auth/crud.js");
 
-exports.start = server.listen(port, function() {
+// Files
+const ServerFunc = require("./functions");
+
+// Extra
+const port = 455;
+const server = new Net.Server();
+exports.Socket_Info = {
+    "SocketIP": "",
+    "SocketPORT": "",
+    "UserIP": "",
+    "UserPORT": ""
+}
+
+// Exports
+exports.svr = server;
+
+server.listen(port, function() {
     console.log("Server started! => "  + port);
-    reset_sessions();
+    Crud.resetSessions();
 });
