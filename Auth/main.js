fd77fa295d23508cbaa343b5b3cb13c91a716783
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-27 09:06:01 -0500

    Update main.js

---
+++ Auth/main.js
@@ -0,0 +1,26 @@
+// Modules
+const fs = require("fs");
+
+// Files
+const Crud = require("./crud.js");
+
+exports.login = function(user, pass) {
+    if(user.length === 0 || pass.length === 0) {
+        return "[x] Error, Invalud argument value!";
+    }
+
+    let get_user = Crud.User(user);
+
+    if(get_user === "[x] Error, No user found!") {
+        return "[x] Error, Username or password seem to be incorrect!. Try again";
+    }
+
+    let db_user = get_user.split(",")[0];
+    let db_pw = get_user.split(",")[2];
+
+    if(user === db_user || pass === db_pw) {
+        return "[+] Successfully logged in, Welcome: " + user;
+    } else {
+        return "[x] Error, Username or password seem to be incorrect!. Try again";
+    }
+}
