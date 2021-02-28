2ae830b2dd40689e5f52f5cb48ed851c6cb474e9
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-28 14:49:57 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -3,9 +3,40 @@ const fs = require("fs");
 const prompt = require('prompt-sync')();
 
 // Files
+const Crud = require("./CNC/Auth/crud.js");
+const eCrud = require("./CNC/Auth/functions.js");
+const Auth = require("./CNC/Auth/main.js");
 
-// Extra
+const Banners = require("./CNC/banners/main.js");
 
-var test = prompt("TEST: ");
+const Config = require("./CNC/Config/main.js");
+const eConfig = require("./CNC/Config/current.js");
 
-console.log(test);
+var adminConfig = {
+    "hostname": Config.Colors.Red + "[" + Config.Colors.Yellow + "Traumatized" + Config.Colors.Red + "@" + Config.Colors.Yellow + "CP" + Config.Colors.Red + "]" + Config.Colors.Yellow + "#~ " + Config.Colors.Cyan
+}
+
+console.clear();
+
+while(true) {
+    var inputCMD = prompt(adminConfig.hostname);
+    let cmd = "";
+    let cmd_argv = "";
+
+    if(inputCMD.includes(" ")) {
+        cmd_argv = inputCMD.split(" ");
+        cmd = cmd_argv[0];
+    } else {
+        cmd = inputCMD;
+        cmd_argv[0] = inputCMD;
+    }
+
+    if(cmd === "help") {
+        console.clear();
+        console.log(Banners.main_b() + Banners.admin_list());
+    } else if(cmd.length === 0) {
+
+    } else {
+        console.log("[x] Error, No command found!");
+    }
+}
