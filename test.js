b20e527d703666166c05d39a8d2ede4281b7fe2a
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-03-01 19:07:13 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-const eCrud = require("./Auth/functions.js");
-const Crud = require("./Auth/crud.js");
+const eCrud = require("./CNC/Auth/functions.js");
+const Crud = require("./CNC/Auth/crud.js");
 
-console.log(eCrud.PremiumCount());
+const Table = require("./CNC/banners/table_creator/main.js");
 
-console.log(Crud.User("nano"))
+// console.log(eCrud.PremiumCount());
 
-console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
+// console.log(Crud.User("nano"))
+
+Table.setColumns([14, 40]);
+console.log(Table.createHeader["test", "this"]);
+
+// console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
