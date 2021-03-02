c788f9a5c54049d2cb7f47028df6388ff89f8ebe
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-03-02 10:57:33 -0500

    Update main.js

--- CNC_CP/main.js
+++ CNC_CP/main.js
@@ -1,9 +1,35 @@
 // Modules
 const fs = require("fs");
+const exec = require("child_process");
 
 // Files
-const Crud = require("./CNC/Auth/crud.js");
+const Crud = require("../CNC/Auth/crud.js");
 
-exports.is_CNC_up = function() {
-    
+exports.cnc_toggle = function(switc) {
+    if(switc === "on") {
+        Turn_CNC_On();
+    } else {
+        Turn_CNC_Off();
+    }
+}
+
+exports.restart_cnc = function() {
+    this.Turn_CNC_Off();
+    this.Turn_CNC_On();
+}
+
+exports.Turn_CNC_On = function() {
+    exec("screen -dmS cnc node cnc.js", function(error, stdout, stderr) {
+        if(!error && !stderr) {
+            console.log("[+] CNC is now starting up on port 455!");
+        }
+    })
+}
+
+exports.Turn_CNC_Off = function() {
+    exec("screen -r cnc -X quit", function(error, stdout, stderr) {
+        if(!stderr && !error) {
+            console.log("[+] CNC is turning off...");
+        }
+    })
 }
