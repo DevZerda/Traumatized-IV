ddabb40cb4e24f081423522c337ec533f390f1bf
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-03-03 13:27:16 -0500

    Update crud.js

--- CNC/Auth/crud.js
+++ CNC/Auth/crud.js
@@ -11,7 +11,6 @@ const Crud = require("./crud.js");
 */
 exports.User = function(user) {
     let db = fs.readFileSync("./CNC/db/sys/users.db", "utf8");
-    console.log(db);
     let users = db.split("\n");
 
     let found_check = false;
@@ -43,9 +42,9 @@ exports.addUser = function(user, ip, password, level, maxtime, admin) {
     let get_user = Crud.User(user);
     if(get_user === "[x] Error, No user found!") {
         fs.appendFile("./CNC/db/sys/users.db", "('" + user + "','" + ip + "','" + password + "','" + level + ",'" + maxtime + "','" + admin + "')\n");
-        return "[+] User: " + user + " successfully added!";
+        return "[+] User: " + user + " successfully added!\r\n";
     } else {
-        return "[x] Error, This username is taken, choose another username!";
+        return "[x] Error, This username is taken, choose another username!\r\n";
     }
 }
 
@@ -57,13 +56,18 @@ exports.removeUser = function(user) {
     let db = fs.appendFileSync("./CNC/db/sys/users.db", "utf8");
     let users = db.split("\n");
 
+    new_db = "";
+
     users.forEach(e => {
         if(e.length > 5) {
-            if(e.includes(user)) {
-
-            }
+            if(!e.includes(user)) {
+                new_db = e + "\n";
+            } 
         }
     })
+
+    fs.writeFileSync("./CNC/db/sys/users.db", new_db);
+    return "[+] User: " + user + " successfully removed!\r\n";
 }
 
 /*
@@ -91,7 +95,7 @@ exports.userUpdate = function(user, new_level, new_maxtime, new_admin) {
 
     fs.writeFileSync("./CNC/db/sys/users.db", new_db);
     console.log("User: " + user + " updated ip!")
-    return "[+] User: " + user + " successfully updated!";
+    return "[+] User: " + user + " successfully updated!\r\n";
 }
 
 exports.changeIP = function(userOrip, ip) {
@@ -175,8 +179,8 @@ exports.resetSessions = function() {
 }
 
 exports.change_motd = function(new_motd) {
-    fs.writeFileSync("./CNC/db/sys/motd.dat", motd);
-    return "[+] Message Of The Day successfully updated!";
+    fs.writeFileSync("./CNC/db/sys/motd.dat", new_motd);
+    return "[+] Message Of The Day successfully set (" + new_motd + ")\r\n";
 }
 
 exports.GetCurrentUser = function(user) {
@@ -201,4 +205,11 @@ exports.GetCurrentUser = function(user) {
     } else {
         return user_line;
     }
+}
+
+exports.resetIP = function(user) {
+    let info = Crud.User(user).split(",");
+    Crud.removeUser(user);
+    Crud.addUser(info[0], info[2], info[3], info[4], info[5])
+
 }
