f9233de0d919fd33019b4e9d926fb4d32cc3d914
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-03-03 13:28:26 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const Crud = require("./CNC/Auth/crud.js");
 
 const Table = require("./CNC/banners/table_creator/main.js");
 
-console.log(eCrud.PremiumCount());
+// console.log(eCrud.PremiumCount());
 
-console.log(Crud.User("nano"))
+// console.log(Crud.User("nano"))
 
-// console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
+console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
