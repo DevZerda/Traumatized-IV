407ba6cd5a8afba207e17345f79aca0b44518370
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-04-13 11:08:28 -0400

    Update cnc.js

--- cnc.js
+++ cnc.js
@@ -40,7 +40,7 @@ Server.svr.on('connection', async function(socket) {
     /* Showing the connecting user on server side terminal! */
     console.log('A new connection has been established\r\nClient IP: ' + Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT + "\r\n");
 
-    socket.write(Config.Colors.Clear);
+
     
     //* END
 
@@ -53,25 +53,25 @@ Server.svr.on('connection', async function(socket) {
     socket.write(Banners.login_b());
     eExtra.set_cursor(9, 37, socket);
     eExtra.set_Title("                                                  FloodSec VI | Welcome to bypass land | [APIs]: 6", socket);
-    let username = await ServerFunc.getInput(socket, "");
+    eConfig.CurrentLogin.Username = await ServerFunc.getInput(socket, "");
 
     socket.write(Config.Colors.Clear);
 
     //Get Password
     socket.write(Banners.login_v());
     eExtra.set_cursor(9, 37, socket);
-    let password = await ServerFunc.getInput(socket, "");
+    eConfig.CurrentLogin.Password = await ServerFunc.getInput(socket, "");
 
     /* Get User Input In A Loop */
 
     socket.write(Config.Colors.Clear);
-    let login_resp = Auth.login(username, password, Server.Socket_Info.UserIP);
+    let login_resp = Auth.login(eConfig.CurrentLogin.Username, eConfig.CurrentLogin.Password, Server.Socket_Info.UserIP);
     if(login_resp.includes("Successfully")) {
         socket.write(Banners.main_b());
-        socket.write("                   Welcome To FloodSec Bypass Land, " + username + "\r\n");
-        eExtra.log_action("Login Attempt Successfully", username, Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT);
+        socket.write("                   Welcome To FloodSec Bypass Land, " + eConfig.CurrentLogin.Username + "\r\n");
+        eExtra.log_action("Login Attempt Successfully", eConfig.CurrentLogin.Username, Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT);
     } else {
-        eExtra.log_action("Login Attempt Failed", username, Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT);
+        eExtra.log_action("Login Attempt Failed", eConfig.CurrentLogin.Username, Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT);
         socket.write(Config.Colors.Clear + "Error, Invalid Info!");
         await eExtra.sleep(3000).then(() => {
             socket.destroy();
@@ -84,9 +84,8 @@ Server.svr.on('connection', async function(socket) {
     *                               GETTING USER INPUT
     */
 
-
     /* End Of User Connecting Process */
-    socket.write(Config.hostname(username))
+    socket.write(Config.hostname(eConfig.CurrentLogin.Username))
     socket.on('data', async function(data) {
         socket.setEncoding('utf8');
         Server.setInfo(socket.remoteAddress.replace("::ffff:", ""), socket.remotePort);
@@ -109,7 +108,12 @@ Server.svr.on('connection', async function(socket) {
             socket.write(result + Config.hostname(Current[0]));
         } else if(eConfig.CurrentCmd.Cmd === "myinfo") {
             socket.write(eCrud.show_stats(Current[0]) + Config.hostname(Current[0]))
+        } else if(eConfig.CurrentCmd.Cmd === "change_pw") {
+            socket.write(Crud.changePassword(Current[0], eConfig.CurrentCmd.arg[1]) + Config.hostname(Current[0]));
         } else if(eConfig.CurrentCmd.Cmd === "stress") {
+            /*
+            *      STRESSER SECTION
+            */
             console.log(eConfig.CurrentCmd.arg);
             socket.write(await eExtra.send_attack(eConfig.CurrentCmd.arg[1], eConfig.CurrentCmd.arg[2], eConfig.CurrentCmd.arg[3], eConfig.CurrentCmd.arg[4]) + Config.hostname(Current[0]));
         } else if(eConfig.CurrentCmd.Cmd === "methods") {
@@ -125,6 +129,9 @@ Server.svr.on('connection', async function(socket) {
                 socket.write(Config.Colors.Clear + Banners.main_b() + Banners.methods() + Config.hostname(Current[0]));
             }
         } else if(eConfig.CurrentCmd.Cmd === "admin") {
+            /*
+            *       ADMIN SECTION 
+            */
             if(eCrud.isAdmin(Current[0]) || eConfig.CurrentUser.isAdmin === true) {
                 let tool = eConfig.CurrentCmd.arg[1];
                 let arg2 = eConfig.CurrentCmd.arg[2];
@@ -157,7 +164,7 @@ Server.svr.on('connection', async function(socket) {
             socket.write("[x] Error, No command found!\r\n" + Config.hostname(Current[0]));
         }
         
-        eExtra.log_action("CMD", username, Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT);
+        eExtra.log_action("CMD", Current[0], Server.Socket_Info.UserIP + ":" + Server.Socket_Info.UserPORT);
     })
         
 
