f6545919d5bf172e6ac87061318fdf45cb448afb
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-04-17 18:09:06 -0400

    Update test.js

--- test.js
+++ test.js
@@ -15,6 +15,7 @@ lul = test.GetRoot("UDP");
 
 // console.log(eCrud.PremiumCount());
 
+
 // console.log(Crud.User("nano"))
 
 // console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
