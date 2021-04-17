c9959273f853d15bf78f23acd3451d8417e8bd32
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-04-17 10:11:27 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,18 @@ const Crud = require("./CNC/Auth/crud.js");
 
 const Table = require("./CNC/banners/table_creator/main.js");
 
+const test = require("./CNC/bot_handler/ssh.js");
+
+lul = test.GetRoot("UDP");
+
+// if(lul.indexOf("\n")==-1) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
 // console.log(eCrud.PremiumCount());
 
 // console.log(Crud.User("nano"))
 
-console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
+// console.log(Crud.userUpdate(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
