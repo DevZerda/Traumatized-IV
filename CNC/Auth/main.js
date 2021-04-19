43cc5ce72dfa95d5ab83614d59d4613764a344bd
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-04-19 12:34:18 -0400

    Update main.js

--- CNC/Auth/main.js
+++ CNC/Auth/main.js
@@ -12,11 +12,13 @@ const eExtra = require("../Extra/functions.js");
 *@type: [<string>]
 */
 exports.login = function(user, pass, ip) {
+    console.log("user, pass, ip:15 [/Auth/crud.js] - " + user + " " + pass + " " + ip);
     if(user.length === 0 || pass.length === 0) {
         return "[x] Error, Invalud argument value!";
     }
 
     let get_user = Crud.User(user);
+    console.log("get_user:20 [/Auth/crud.js] - " + get_user);
     if(get_user === "[x] Error, No user found!") {
         return "[x] Error, Username or password seem to be incorrect!. Try again";
     }
